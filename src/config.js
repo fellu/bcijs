480beb282dec4563c31d06ee6837ae01b862356c
Felix Ippolitov <fellu@f-solutions.fi>
2018-04-30 23:25:19 +0300

    BCI

--- src/config.js
+++ src/config.js
@@ -4,20 +4,20 @@ config.js - Configuration for ZENCash Coin
 
 module.exports = {
   mainnet: {
-    messagePrefix: 'ZENCash main net',
+    messagePrefix: 'BCI main net',
     bip32: {
       public: '0488b21e',
       private: '0488ade4'
     },
-    pubKeyHash: '2089',
-    scriptHash: '2096',
+    pubKeyHash: '66',
+    scriptHash: '17',
     zcPaymentAddressHash: '169a', // Private z-address
     zcSpendingKeyHash: 'ab36', // Spending key
     wif: '80'
   },
   testnet: {
     wif: 'ef',
-    pubKeyHash: '2098',
-    scriptHash: '2092'
+    pubKeyHash: '66',
+    scriptHash: '17'
   }
 }
